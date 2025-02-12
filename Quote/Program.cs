9009b894668594a91b3cb7ca95e0021f3c5762ca
daniil <skrimexgame12508@mail.ru>
2025-02-12 20:44:01 +0300

    Добавьте файлы проекта.

---
+++ Quote/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuoteServer
+{
+    internal class Program
+    {
+        private const int PORT = 5555;
+        private static List<string> quotes = new List<string>
+        {
+            "Не ешь желтый снег",
+            "Носи двое штанов зимой",
+            "Без труда не выловишь и рыбку и пруда",
+            "Когда рак на горе свистнет"
+        };
+
+        //запуск сервера
+        public static async Task StartServerAsync()
+        {
+            try
+            {
+                TcpListener tcp = new TcpListener(System.Net.IPAddress.Any, PORT);
+                tcp.Start();
+                Console.WriteLine("Сервер запущен. Ожидаем подключения клиентов");
+
+                while (true)
+                {
+                    TcpClient client = await tcp.AcceptTcpClientAsync();
+
+                    await HandleClientAsync(client).ContinueWith(t =>
+                    {
+                        if (t.IsFaulted)
+                        {
+                            Console.WriteLine($"Ошибка обработки " +
+                                $"запроса от клиента. {t.Exception}");
+                        }
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка сервера: " + ex.Message);
+            }
+
+        }
+
+        private static async Task HandleClientAsync(TcpClient client)
+        {
+            NetworkStream stream = null;
+            string clientId = Guid.NewGuid().ToString().Substring(0, 8);
+
+            try
+            {
+                stream = client.GetStream();
+                bool isConected = true;
+                LogEvent($"Клиент {clientId} подключился.");
+                int countRequest = 0;
+                while (isConected && countRequest < 3)
+                {
+                    byte[] buffer = new byte[256];
+                    int bytesRead = await stream
+                        .ReadAsync(buffer, 0, buffer.Length);
+
+                    string request = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    byte[] responseBytes = null;
+                    string response = "";
+                    if (request.Equals("quote")) 
+                    {
+                        response = GetRandomQuote();                  
+                        LogEvent($"Клиенту {clientId} отправлена цитата {response}");                    
+                        countRequest++;
+
+                        if (countRequest == 3)
+                        {
+                            response = "Закончен лимит цитат ";
+                            Console.WriteLine("Закончен лимит");
+                        }
+                    }
+                    else if(request.Equals("exit"))
+                    {
+                        isConected = false;
+                        LogEvent($"Клиент {clientId} отправил запрос на отключение");
+                    }
+                    else
+                    {
+                        response = "Запрос некорректный. " +
+                            "Отправьте \"qoute\" или \"exit\"";               
+                        await stream.WriteAsync(responseBytes, 0, responseBytes.Length);
+
+                        LogEvent($"Клиент {clientId} отправил неверный запрос");
+                    }
+
+                    responseBytes = Encoding.UTF8.GetBytes(response);
+                    await stream.WriteAsync(responseBytes, 0, responseBytes.Length);
+                }
+            }
+            catch(Exception ex)
+            {
+                LogEvent($"Ошибка обработки клиента {client}: {ex.Message}");
+            }
+            finally
+            {
+                stream.Close();
+                client.Close();
+                LogEvent($"Клиент {clientId} отключился");
+            }
+        }
+
+        private static string GetRandomQuote()
+        {
+            Random random = new Random();
+            return quotes[random.Next(quotes.Count)];
+        }
+
+        private static void LogEvent(string message)
+        {
+            string filePath = "C:/top/log.txt";
+            string logEntry = $"{DateTime.Now}: {message}";
+
+            File.AppendAllText(filePath, logEntry + Environment.NewLine);
+            Console.WriteLine(logEntry);
+        }
+
+        static async Task Main(string[] args)
+        {
+            await StartServerAsync();
+        }
+    }
+}
---
+++ QuoteClient/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuoteClient
+{
+    internal class Program
+    {
+        private const string serverIp = "127.0.0.1";
+        private const int port = 5555;
+
+        public static async Task RunClientAsync()
+        {
+            try
+            {
+                using (TcpClient client = new TcpClient())
+                {
+                    Console.WriteLine("Подключение к серверу...");
+                    await client.ConnectAsync(serverIp, port);
+                    Console.WriteLine("Подключение установлено");
+
+                    NetworkStream stream = client.GetStream();
+
+                    while(true)
+                    {
+                        Console.WriteLine("Введите команду (quote или exit)");
+                        string command = Console.ReadLine().Trim().ToLower();
+
+                        byte[] requestBytes = Encoding.UTF8.GetBytes(command);
+                        await stream.WriteAsync(requestBytes, 0, requestBytes.Length);
+
+                        byte[] buffer = new byte[1024];
+                        int readBytes = await stream.ReadAsync(buffer, 0, buffer.Length);
+
+                        string quote = Encoding.UTF8.GetString(buffer, 0, readBytes);
+
+                        if (command.Equals("exit"))
+                            break;
+
+                        if (command.Equals("quote")) {     
+                            Console.WriteLine(quote);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Неверная команда");
+                        }
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        static async Task Main(string[] args)
+        {
+            await RunClientAsync();
+        }
+    }
+}
